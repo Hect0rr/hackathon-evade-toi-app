5e7a88d65303f69402947f17ed50e46d3573e5e5
Yan Lamothe <yan.santerre@gmail.com>
2019-11-22 23:40:17 -0500

    setup header

---
+++ src/components/Main.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section>
+    <h1>Hackaton Évade Toi Place Holder</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'Main',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+</style>
